98e951dab2dd86542f4bd9fa3a6b71093752af66
Maxym Pavlyuk <Maxym Pavluyk>
2013-05-20 15:09:24 +0300

    fixing metadata

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,9 @@
-name: "aide"
+name: "chef-aide"
 description: "Installs and configures AIDE HIDS"
 long_description: ""
 maintainer: "Elliot Kendall"
 maintainer_email: "elliot.kendall@ucsf.edu"
 license: "BSD"
 version: "0.1.2"
+
+depends "ruby"
